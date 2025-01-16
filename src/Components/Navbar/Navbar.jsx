da0192b5054e44b7f2668a928d548257b51774f2
Praveen Dilshan Rosa <125847850+PraveenRosa2001@users.noreply.github.com>
2025-01-17 00:45:54 +0530

    Finalize front end

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <Link to='/login'><button>Login</button></Link>
         <Link to='/cart'><img src={cart_icon} alt='' className="cart-icon" /></Link>
-        <div className='nav-cart-count'>{getTotalCartItems}</div>
+        <div className='nav-cart-count'>{getTotalCartItems()}</div>
       </div>
     </div>
   );
